e25192e4064ea12c81fba9597b2c49caaed83904
韩槟 <974267095@qq.com>
2020-08-28 13:58:55 +0800

    功能更新
    增加了结果组件
    修改了部分显示效果

---
+++ components/result/index.js
@@ -0,0 +1,17 @@
+Component({
+	options: {
+		addGlobalClass: true
+	},
+	properties: {
+		status: String,
+		title: String,
+		subtitle: String,
+		useExtraSlot: Boolean
+	},
+	data: {
+
+	},
+	methods: {
+
+	}
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,4 @@
-// pages/index/index.js
 Page({
-
-	/**
-	 * 页面的初始数据
-	 */
 	data: {
 		list: [{
 			title: "输入框垂直排列",
@@ -31,65 +26,14 @@ Page({
 			url: "/pages/descriptions/index",
 			border: true
 		}, {
-			title: "状态标签",
+			title: "状态标签（组件）",
 			label: "Status",
-			url: "/pages/status/index"
+			url: "/pages/status/index",
+			border: true
+		}, {
+			title: "结果（组件）",
+			label: "Result",
+			url: "/pages/result/index"
 		}]
-	},
-
-	/**
-	 * 生命周期函数--监听页面加载
-	 */
-	onLoad: function (options) {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
-
-	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
-
-	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
-
-	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
-
 	}
 })
